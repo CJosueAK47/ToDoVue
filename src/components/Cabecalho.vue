0b03cced0f7e94f03c3fc075522206fd000956e9
CJosueAK47 <carlos.jdc@discente.ufma.br>
2023-10-18 09:37:40 -0300

    subindo

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,11 @@
+<script setup>
+    const props = defineProps(['tarefasPendentes'])
+</script>
+<template>
+    <header class="p-5 mb-4 mt-4 bg-light rouded-3">
+        <h1>Minhas Tarefas</h1>
+        <p>
+            Você possui {{ props.tarefasPendentes }} caralhada de tarefas
+        </p>
+    </header>
+</template>
